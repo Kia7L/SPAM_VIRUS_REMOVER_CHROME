e343fbf0a664883962217617256bee217e1e22b6
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-02-11 08:07:07 -1000

    Create SOUNDviral.bat

---
+++ FixMUSIC/SOUNDviral.bat
@@ -0,0 +1,34 @@
+cd /
+mod 50,11
+echo off
+Title "FIX MUZIX 7L"
+cls
+@color 04
+echo. FIXERTUNER GLITCH.. BY KIA
+(ping -n 1 192.168.1.1)>NUL
+(timeout /t 1)>NUL
+cecho.exe {09}
+echo. Finishing up...
+(ping -n 2 192.144.1.1)>NUL
+(timeout /t 2)>NUL
+cls
+@color 04
+echo. FIXERTUNER GLITCH.. BY KIA
+cecho.exe {09}
+echo. Finishing up..
+(timeout /t 1)>NUL
+cls
+@color 04
+echo. FIXERTUNER GLITCH.. BY KIA
+cecho.exe {09}
+echo. Finishing up.
+(timeout /t 1)>NUL
+cls
+@color 04
+echo. FIXERTUNER GLITCH.. BY KIA
+cecho.exe {09}
+echo. Finishing up
+(timeout /t 2)>NUL
+exit
+
+
