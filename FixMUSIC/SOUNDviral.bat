44d581daeb85bfac5c9ee029ad17b2a7160709d8
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-03-09 05:26:44 -1000

    Update SOUNDviral.bat

--- FixMUSIC/SOUNDviral.bat
+++ FixMUSIC/SOUNDviral.bat
@@ -6,29 +6,16 @@ cls
 @color 04
 echo. FIXERTUNER GLITCH.. BY KIA
 (ping -n 1 192.168.1.1)>NUL
-(timeout /t 1)>NUL
+  (sleep 2)>NUL
 cecho.exe {09}
 echo. Finishing up...
-(ping -n 2 192.144.1.1)>NUL
-(timeout /t 2)>NUL
-cls
-@color 04
-echo. FIXERTUNER GLITCH.. BY KIA
+(ping -n 4 192.144.1.1)>NUL
+(sleep 2)>NUL
 cecho.exe {09}
 echo. Finishing up..
-(timeout /t 1)>NUL
 cls
-@color 04
-echo. FIXERTUNER GLITCH.. BY KIA
 cecho.exe {09}
 echo. Finishing up.
-(timeout /t 1)>NUL
-cls
-@color 04
-echo. FIXERTUNER GLITCH.. BY KIA
-cecho.exe {09}
-echo. Finishing up
-(timeout /t 2)>NUL
 exit
 
 
